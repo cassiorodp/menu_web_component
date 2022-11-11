d68eecb8377487056fc1b7cdd5dca63d73b4ef35
Cássio Pereira <cassiorodp@gmail.com>
2022-11-11 11:32:53 -0300

    feat: add gsap effect

--- src/components/MenuComponent/MenuComponent.jsx
+++ src/components/MenuComponent/MenuComponent.jsx
@@ -1,6 +1,8 @@
 import { css } from 'goober';
+import gsap from 'gsap';
+import { useRef } from 'preact/hooks';
 
-let wrapper = (props) => {
+let mainWrapper = (props) => {
   return css({
     position: 'fixed',
     top: props.ydisplacement,
@@ -9,6 +11,43 @@ let wrapper = (props) => {
   });
 };
 
+let buttonWrapper = (props) => {
+  return css({
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    backgroundColor: props.backgroundcolor,
+    zIndex: 1,
+  });
+};
+
+let divWrapper = css`
+  width: 100px;
+  height: 100px;
+  background-color: red;
+  position: absolute;
+  top: 0;
+  left: 0;
+  transform: scale(0);
+  transform-origin: top left;
+  z-index: 2;
+`;
+
 export const MenuComponent = (props) => {
-  return <button className={wrapper(props)}>MENU</button>;
+  const divRef = useRef(null);
+
+  const openMenu = () => {
+    gsap.to(divRef.current, {
+      scale: 1,
+    });
+  };
+
+  return (
+    <div className={mainWrapper(props)}>
+      <div ref={divRef} className={divWrapper} />
+      <button onClick={openMenu} className={buttonWrapper(props)}>
+        MENU
+      </button>
+    </div>
+  );
 };
