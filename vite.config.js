efb5fc69a8349cd7f926d038ea99897b6ce36caa
Cássio Pereira <cassiorodp@gmail.com>
2022-11-11 09:38:32 -0300

    fix: rename filename

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
-import { defineConfig } from "vite";
-import { resolve } from "path";
-import preact from "@preact/preset-vite";
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+import preact from '@preact/preset-vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,10 +8,10 @@ export default defineConfig({
 
   build: {
     lib: {
-      entry: resolve(__dirname, "src/main.jsx"),
-      name: "MyComponent",
+      entry: resolve(__dirname, 'src/main.jsx'),
+      name: 'MenuComponent',
       // the proper extensions will be added
-      fileName: "my-component",
+      fileName: 'menu-component',
     },
   },
 });
